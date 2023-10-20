95b9e0f9c8d8e34aaa36dfeb7c12ae769ce98fa9
KrsNovita2717 <novitakristiana2717@gmail.com>
2023-10-20 14:19:22 +0700

    test Scenario A2

--- tests/likeMovie.test.js
+++ tests/likeMovie.test.js
@@ -13,4 +13,15 @@ describe('Liking A Movie', () => {
 
     expect(document.querySelector('[aria-label="like this movie"]')).toBeTruthy();
   });
+
+  it('should not show the unlike button when the movie has not been liked before', async () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+    expect(document.querySelector('[aria-label="unlike this movie"]')).toBeFalsy();
+  });
 });
