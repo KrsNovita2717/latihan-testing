1a7ab8e7f28023cdbb5a9b108d32ffbcc7c1b8b7
KrsNovita2717 <novitakristiana2717@gmail.com>
2023-10-20 16:25:29 +0700

    ToDo 3

--- tests/favoriteMovieSearch.test.js
+++ tests/favoriteMovieSearch.test.js
@@ -3,6 +3,15 @@ import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
 import FavoriteMovieSearchPresenter from '../src/scripts/views/pages/liked-movies/favorite-movie-search-presenter';
 
 describe('Searching movies', () => {
+  let presenter;
+
+  const searchMovies = (query) => {
+    const queryElement = document.getElementById('query');
+    queryElement.value = query;
+
+    queryElement.dispatchEvent(new Event('change'));
+  };
+
   beforeEach(() => {
     document.body.innerHTML = `
       <div id="movie-search-container">
@@ -13,29 +22,18 @@ describe('Searching movies', () => {
         </div>
       </div>
     `;
+
+    spyOn(FavoriteMovieIdb, 'searchMovies');
+    presenter = new FavoriteMovieSearchPresenter({ favoriteMovies: FavoriteMovieIdb });
   });
 
   it('should be able to capture the query typed by the user', () => {
-    spyOn(FavoriteMovieIdb, 'searchMovies');
-    const presenter = new FavoriteMovieSearchPresenter({ favoriteMovies: FavoriteMovieIdb });
-
-    const queryElement = document.getElementById('query');
-    queryElement.value = 'film a';
-
-    queryElement.dispatchEvent(new Event('change'));
+    searchMovies('film a');
     expect(presenter.latestQuery).toEqual('film a');
   });
 
   it('should ask the model to search for liked movies', () => {
-    spyOn(FavoriteMovieIdb, 'searchMovies');
-
-    // eslint-disable-next-line no-unused-vars
-    const presenter = new FavoriteMovieSearchPresenter({ favoriteMovies: FavoriteMovieIdb });
-
-    const queryElement = document.getElementById('query');
-    queryElement.value = 'film a';
-
-    queryElement.dispatchEvent(new Event('change'));
+    searchMovies('film a');
 
     expect(FavoriteMovieIdb.searchMovies).toHaveBeenCalledWith('film a');
   });
